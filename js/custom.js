cf488806c9c559a8414fae061215b1822b637dbb
Preston Stosur-Bassett <preston@stosur.info>
2014-12-10 00:21:56 -0500

    working contenthover

--- js/custom.js
+++ js/custom.js
@@ -212,10 +212,10 @@ $(document).ready(function() {
 	//Pop2 Brooklyn
 	$("#pop2_brooklyn").contenthover({
     overlay_width:270,
-		overlay_height:180,
-		effect:'slide',
-		slide_direction:'right',
-		overlay_x_position:'right',
+		overlay_height:300,
+		//effect:'slide',
+		//slide_direction:'top',
+		overlay_x_position:'top',
 		overlay_y_position:'center',
 		overlay_background:'#000',
 		overlay_opacity:0.8
